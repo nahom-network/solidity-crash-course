e0d08dacc867525432bbf3c1d878e168a02cb770
firaolkiya <firaolkiya6@gmail.com>
2026-05-04 11:00:00 -0300

    contrat

--- Lesson-3/Mappings/Is_Member.sol
+++ Lesson-3/Mappings/Is_Member.sol
@@ -1,17 +1,63 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+/**
+ * @title Membership Contract
+ * @dev Manages a membership registry using mappings
+ * @notice Allows adding, removing, and checking membership status
+ */
 contract Contract {
-
+    /// @notice Mapping to track membership status of each address
     mapping(address => bool) public members;
+    
+    /// @notice Admin address with special privileges
+    address public admin;
+    
+    /// @notice Total number of active members
+    uint public memberCount;
+    
+    /// @notice Event emitted when a member is added
+    event MemberAdded(address indexed user);
+    
+    /// @notice Event emitted when a member is removed
+    event MemberRemoved(address indexed user);
+    
+    constructor() {
+        admin = msg.sender;
+    }
 
+    /**
+     * @notice Add a user to the membership registry
+     * @param user The address to add as a member
+     */
     function addMember(address user) external {
+        require(msg.sender == admin, "Only admin can add members");
+        require(user != address(0), "Invalid address");
+        require(!members[user], "User is already a member");
+        
         members[user] = true;
+        memberCount++;
+        emit MemberAdded(user);
     }
 
+    /**
+     * @notice Remove a user from the membership registry
+     * @param user The address to remove from membership
+     */
     function removeMember(address user) external {
+        require(msg.sender == admin, "Only admin can remove members");
+        require(members[user], "User is not a member");
+        
         members[user] = false;
+        memberCount--;
+        emit MemberRemoved(user);
     }
 
+    /**
+     * @notice Check if a user is a member
+     * @param user The address to check
+     * @return bool True if the user is a member, false otherwise
+     */
     function isMember(address user) external view returns (bool) {
         return members[user];
     }
--- Lesson-4/Inheritance/Inherit.sol
+++ Lesson-4/Inheritance/Inherit.sol
@@ -1,17 +1,52 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
 import "./Hero.sol";
 
+/**
+ * @title Mage Contract
+ * @dev Represents a Mage hero with spell attack capabilities
+ * @notice Inherits from Hero and overrides the attack function
+ */
 contract Mage is Hero(50) {
+    /// @notice The attack type for this mage
+    string public constant ATTACK_TYPE = "Spell";
+    
+    /// @notice Number of spells cast by this mage
+    uint public spellsCast;
+    
+    /**
+     * @notice Execute a spell attack on an enemy
+     * @param enemy The target enemy to attack
+     * @dev Overrides Hero's attack function with spell-specific behavior
+     */
     function attack(Enemy enemy) public override {
         enemy.takeAttack(AttackTypes.Spell);
+        spellsCast++;
         super.attack(enemy);
     }
 }
 
+/**
+ * @title Warrior Contract
+ * @dev Represents a Warrior hero with melee attack capabilities
+ * @notice Inherits from Hero and overrides the attack function
+ */
 contract Warrior is Hero(200) {
+    /// @notice The attack type for this warrior
+    string public constant ATTACK_TYPE = "Brawl";
+    
+    /// @notice Number of brawls engaged in by this warrior
+    uint public brawlsEngaged;
+    
+    /**
+     * @notice Execute a brawl attack on an enemy
+     * @param enemy The target enemy to attack
+     * @dev Overrides Hero's attack function with melee-specific behavior
+     */
     function attack(Enemy enemy) public override {
         enemy.takeAttack(AttackTypes.Brawl);
+        brawlsEngaged++;
         super.attack(enemy);
     }
 }
