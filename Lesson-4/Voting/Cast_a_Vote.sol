022b270151eb14be44ad9ab284b5c1653455f9f6
jerry22-tedi <tedelaalbenejerry@gmail.com>
2026-05-07 00:54:07 +0300

    feat: add Lesson 1-4 Solidity exercises and course completion screens…

--- Lesson-4/Voting/Cast_a_Vote.sol
+++ Lesson-4/Voting/Cast_a_Vote.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
 contract Voting {
@@ -11,6 +12,7 @@ contract Voting {
 
     Proposal[] public proposals;
 
+    // Creates a new proposal and adds it to the list of proposals
     function newProposal(address target, bytes calldata data) external {
         proposals.push(
             Proposal({
@@ -22,7 +24,7 @@ contract Voting {
         );
     }
 
-    // Cast a vote on a proposal
+    // Cast a vote on a proposal (true for yes, false for no)
     function castVote(uint proposalId, bool support) external {
         Proposal storage proposal = proposals[proposalId];
 
