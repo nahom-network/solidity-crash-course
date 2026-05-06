adb32c2a979f7c9985ed42d50e00034cb1f277fb
jerry22-tedi <tedelaalbenejerry@gmail.com>
2026-05-07 00:08:34 +0300

    commenting functionality

--- BlockChain_Solidity_Course/Solidity Introduction/Functions/Arguments.sol
+++ BlockChain_Solidity_Course/Solidity Introduction/Functions/Arguments.sol
@@ -4,14 +4,17 @@ pragma solidity ^0.8.20;
 contract Contract {
     uint public x;
 
+    // Initializes the contract with a starting value for x
     constructor(uint _x) {
         x = _x;
     }
 
+    // Increases the value of x by 1
     function increment() external {
         x += 1;
     }
 
+    // Returns the sum of x and a given value without modifying state
     function add(uint _value) external view returns (uint) {
         return x + _value;
     }
